c214bd16260d28bd8408230b370148248a51e13a
depthbomb <reesepup77@hotmail.com>
2023-02-03 19:37:12 -0600

    refactor: add common package

---
+++ packages/tetra-common/tsup.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'tsup';
+import { builtinModules } from 'node:module';
+
+const production = process.env.NODE_ENV === 'production';
+
+export default defineConfig(() => ({
+	clean: true,
+	entry: ['src/index.ts'],
+	noExternal: [
+		...builtinModules.flatMap(p => [p, `node:${p}`]),
+	],
+	format: ['cjs', 'esm'],
+	minify: production,
+	skipNodeModulesBundle: !production,
+	target: 'esnext',
+	tsconfig: './tsconfig.json',
+	dts: true,
+	splitting: true,
+	sourcemap: true,
+	keepNames: true,
+}));
--- packages/tetra-server/src/database/models/User.ts
+++ packages/tetra-server/src/database/models/User.ts
@@ -1,5 +1,5 @@
 import { model, Schema } from 'mongoose';
-import { UserRole } from '~common/UserRole';
+import { UserRole } from '@tetra/common';
 import type { HydratedDocument } from 'mongoose';
 
 type IUser = {
