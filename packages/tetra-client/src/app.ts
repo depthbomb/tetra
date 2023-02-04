111819276a42294220061b9415622aa968672f62
depthbomb <reesepup77@hotmail.com>
2023-02-03 21:55:05 -0600

    refactor(client): better handle bootstrap failure

--- packages/tetra-client/src/app.ts
+++ packages/tetra-client/src/app.ts
@@ -1,28 +1,25 @@
 import '~/assets/css/app.scss';
-import { createApp }     from 'vue';
-import { createPinia }   from 'pinia';
-import router            from '~/router';
-import App               from '~/App.vue';
+import App from '~/App.vue';
+import router from '~/router';
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import vuetify from '~/plugins/vuetify';
+import hljs from '~/plugins/highlightjs';
 import { useTetraStore } from '~/stores/tetra';
-import vuetify           from '~/plugins/vuetify';
-import hljs              from '~/plugins/highlightjs';
 
 const app = createApp(App)
 	.use(router)
 	.use(vuetify)
 	.use(hljs)
-	.use(createPinia());
+	.use(createPinia())
+	.component('hljs', hljs.component);
 
 // Load backend data into store as early as possible
 const store = useTetraStore();
 const csrfConfigElement = document.querySelector('meta[name="config/csrf-token"]') as HTMLMetaElement;
 if (csrfConfigElement) {
 	store.csrfToken = csrfConfigElement.content;
+	app.mount('#app');
 } else {
-	// TODO handle this better?
-	throw new Error('Failed to load backend data, app element not found.');
+	document.body.innerHTML = '<h1 style="font-size:36px;text-align:center;">Failed to load backend data, app element not found.</h1>';
 }
-
-app.component('hljs', hljs.component);
-
-app.mount('#app');
