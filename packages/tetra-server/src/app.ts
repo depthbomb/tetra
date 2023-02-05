d909202a99179e734d5a80e5f4fc9f48754f7153
Liam Dyer <liamcdyer@gmail.com>
2023-02-04 22:39:30 -0500

    feat(server): allow setting db name via env/config

--- packages/tetra-server/src/app.ts
+++ packages/tetra-server/src/app.ts
@@ -1,13 +1,14 @@
 import 'source-map-support/register';
 import { log } from '~logger';
 import { connect } from 'mongoose';
-import { getEnv, getOrThrow } from '~config';
+import { get, getEnv, getOrThrow } from '~config';
 import { startServer } from '~services/tetra';
 
 const _logger = log.getSubLogger({ name: 'BOOT' });
 const _connectionString = getEnv<string>('TETRA_DATABASE_CONNECTION_STRING') ?? getOrThrow<string>('database.connectionString');
+const _dbName = getEnv<string>('TETRA_DATABASE_NAME') ?? get<string>('database.name');
 
-connect(_connectionString).then(async () => {
+connect(_connectionString, { dbName: _dbName }).then(async () => {
 	_logger.info('Connected to database, starting web service...');
 
 	await startServer();
