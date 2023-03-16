a717d540cf50c4bda0d3b764cba382b13687261b
depthbomb <reesepup77@hotmail.com>
2023-03-15 21:21:27 -0500

    refactor(client): simplify route guards

--- client/src/router/guards/adminGuard.ts
+++ client/src/router/guards/adminGuard.ts
@@ -6,15 +6,11 @@ export function useAdminGuard(): NavigationGuard {
 		window.location.href = '/';
 	};
 	return async (to, from, next) => {
-		try {
-			const { admin } = useUserStore()
-			if (!admin) {
-				redirectToHome();
-			} else {
-				return next();
-			}
-		} catch (err: unknown) {
+		const { admin } = useUserStore()
+		if (!admin) {
 			redirectToHome();
+		} else {
+			return next();
 		}
 	};
 }
--- client/src/router/guards/authGuard.ts
+++ client/src/router/guards/authGuard.ts
@@ -2,20 +2,12 @@ import { useUserStore } from '~/stores/user';
 import type { NavigationGuard } from 'vue-router';
 
 export function useAuthGuard(): NavigationGuard {
-	const alertNoSession = () => {
-		alert('Your session has expired, please log in again.');
-		window.location.href = '/auth/logout';
-	};
 	return async (to, from, next) => {
-		try {
-			const { loggedIn } = useUserStore();
-			if (!loggedIn) {
-				alertNoSession();
-			} else {
-				return next();
-			}
-		} catch (err: unknown) {
-			alertNoSession();
+		const { loggedIn } = useUserStore();
+		if (!loggedIn) {
+			return next(from);
+		} else {
+			return next();
 		}
 	};
 }
