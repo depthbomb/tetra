9da58263bfdc041ee0dff4f86327c7aafe23348f
depthbomb <reesepup77@hotmail.com>
2023-05-01 17:12:42 -0500

    feat: use formatter for error responses

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -1,11 +1,16 @@
 <?php namespace App\Controller;
 
 use Throwable;
+use App\Service\FormatService;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class ErrorController extends BaseController
 {
+    public function __construct(
+        private readonly FormatService $format,
+    ) {}
+
     public function show(Throwable $exception): Response
     {
         $code = Response::HTTP_INTERNAL_SERVER_ERROR;
@@ -15,7 +20,7 @@ class ErrorController extends BaseController
             $code = $exception->getStatusCode();
         }
 
-        return $this->json([
+        return $this->format->formatData([
             'status'  => $code,
             'message' => $exception->getMessage() !== '' ? $exception->getMessage() : Response::$statusTexts[$code],
         ], $code);
