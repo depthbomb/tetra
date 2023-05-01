11737369f08c1ac4e050bc676c4538f6c6ad2487
depthbomb <reesepup77@hotmail.com>
2023-05-01 12:44:53 -0500

    feat: add feedback to copy button, allow custom initial icon

--- assets/src/components/CopyButton.vue
+++ assets/src/components/CopyButton.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
 	import { ref } from 'vue';
 	import { useClipboard } from '@vueuse/core';
+	import { useToastStore } from '~/stores/toast';
 	import AppButton from '~/components/AppButton.vue';
 	import CopyIcon from '~/components/icons/CopyIcon.vue';
 	import CheckIcon from '~/components/icons/CheckIcon.vue';
-
-	const { copy } = useClipboard({ legacy: true });
+	import type { Component } from 'vue';
 
 	const props = defineProps<{
 		text?: string;
+		icon?: Component;
 		content: string;
 		size?: 'small' | 'medium' | 'large';
 	}>();
@@ -19,16 +20,20 @@
 		'mr-2 h-5':     props.size === 'medium' || props.size === 'large',
 	}]);
 
+	const { copy }        = useClipboard({ legacy: true });
+	const { createToast } = useToastStore();
+
 	const doCopyLink = () => {
 		copy(props.content);
 		copiedState.value = true;
 		setTimeout(() => copiedState.value = false, 1_500);
+		createToast('success', 'Copied to clipboard!');
 	};
 </script>
 
 <template>
 	<app-button :variant="copiedState ? 'success' : 'brand'" @click.prevent="doCopyLink">
-		<copy-icon v-if="!copiedState" :class="iconClass"/>
+		<component v-if="!copiedState" :is="icon ?? CopyIcon" :class="iconClass"/>
 		<check-icon v-else :class="iconClass"/>
 		<span v-if="text">{{ text }}</span>
 	</app-button>
