bac7b53a395b188c537dadd3759bc59a2b022399
depthbomb <reesepup77@hotmail.com>
2023-05-03 15:34:03 -0500

    refactor: truncate long destinations in admin

--- assets/src/components/admin/ShortlinkRow.vue
+++ assets/src/components/admin/ShortlinkRow.vue
@@ -4,15 +4,17 @@
 	import TimeAgo from '~/components/TimeAgo.vue';
 	import AppButton from '~/components/AppButton.vue';
 	import type { IAllShortlinksRecord } from '~/@types/IAllShortlinksRecord';
+import { useTruncation } from '~/composables/useTruncation';
 
 	const props = defineProps<{
 		shortlink: IAllShortlinksRecord,
 		shiftKey: boolean;
 	}>();
+	const emit = defineEmits(['shortlink-deleted', 'shortlink-disable-toggled']);
 
 	const disabled = ref(props.shortlink.disabled);
 
-	const emit = defineEmits(['shortlink-deleted', 'shortlink-disable-toggled']);
+	const { truncate } = useTruncation();
 
 	const deleteShortlink = async (shortcode: string, secret: string) => {
 		if (props.shiftKey === true || confirm('Are you sure you want to delete this shortlink?')) {
@@ -24,7 +26,7 @@
 	};
 
 	const toggleShortlinkDisabled = async (shortcode: string) => {
-		if (props.shiftKey === true || confirm('Are you sure you want to disable this shortlink?')) {
+		if (props.shiftKey === true || confirm(`Are you sure you want to ${disabled.value ? 'enable' : 'disable'} this shortlink?`)) {
 
 			const { success } = await useApi(`/api/admin/toggle-shortlink-disabled`, {
 				body: JSON.stringify({ shortcode }),
@@ -45,10 +47,12 @@
 			<app-button variant="danger" size="small" @click.prevent="deleteShortlink(shortlink.shortcode, shortlink.secret)">Delete</app-button>
 			<app-button :variant="disabled ? 'success' : 'warning'" size="small" @click.prevent="toggleShortlinkDisabled(shortlink.shortcode)">{{ disabled ? 'Enable' : 'Disable' }}</app-button>
 		</td>
-		<td>{{ shortlink.shortcode }}</td>
 		<td>
-			<p>
-				<a :href="shortlink.destination" target="_blank">{{ shortlink.destination }}</a>
+			<p class="font-mono">{{ shortlink.shortcode }}</p>
+		</td>
+		<td>
+			<p class="font-mono">
+				<a :href="shortlink.destination" target="_blank">{{ truncate(shortlink.destination, 50) }}</a>
 			</p>
 		</td>
 		<td>{{ shortlink.user_username ?? shortlink.creator_ip }}</td>
