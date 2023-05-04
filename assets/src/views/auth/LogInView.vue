86aacc3ab049b522a660b32ef6acd801512d616a
depthbomb <reesepup77@hotmail.com>
2023-05-03 19:58:10 -0500

    refactor: use loader component for auth routes

--- assets/src/views/auth/LogInView.vue
+++ assets/src/views/auth/LogInView.vue
@@ -1,13 +1,10 @@
 <script setup lang="ts">
 	import { onMounted } from 'vue';
-	import SpinnerIcon from '~/components/icons/SpinnerIcon.vue';
+	import AppLoader from '~/components/AppLoader.vue';
 
 	onMounted(() => window.location.href = '/oidc/start');
 </script>
 
 <template>
-	<div class="flex items-center justify-center space-x-3">
-		<spinner-icon class="animate-spin w-6 h-6"/>
-		<p class="text-xl">Redirecting&hellip;</p>
-	</div>
+	<app-loader text="Redirecting you to login&hellip;"/>
 </template>
--- assets/src/views/auth/LogOutView.vue
+++ assets/src/views/auth/LogOutView.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 	import { onMounted } from 'vue';
 	import { useApi } from '~/composables/useApi';
+	import AppLoader from '~/components/AppLoader.vue';
 	import { useBridge } from '~/composables/useBridge';
-	import SpinnerIcon from '~/components/icons/SpinnerIcon.vue';
 
 	const bridge = useBridge();
 
@@ -15,8 +15,5 @@
 </script>
 
 <template>
-	<div class="flex items-center justify-center space-x-3">
-		<spinner-icon class="animate-spin w-6 h-6"/>
-		<p class="text-xl">Logging you out&hellip;</p>
-	</div>
+	<app-loader text="Logging you out&hellip;"/>
 </template>
