ec32bcdfbc67118c8e438f54ea5c2a6656491f3d
depthbomb <reesepup77@hotmail.com>
2023-05-07 00:12:04 -0500

    refactor: validate JSON request payload

--- src/EventSubscriber/ParseJsonRequestSubscriber.php
+++ src/EventSubscriber/ParseJsonRequestSubscriber.php
@@ -4,6 +4,7 @@ use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
+// TODO remove this subscriber when Symfony 8.3 releases to utilize the new getPayload() method that can parse JSON request payloads
 class ParseJsonRequestSubscriber implements EventSubscriberInterface
 {
 
@@ -20,11 +21,13 @@ class ParseJsonRequestSubscriber implements EventSubscriberInterface
         $request = $event->getRequest();
         if ($request->getContentTypeFormat() === 'json')
         {
-            // TODO validate JSON after migrating to PHP 8.3
+            $json = $request->getContent();
+            if (json_validate($json))
+            {
+                $json_body = json_decode($json, true);
 
-            $json_body = json_decode($request->getContent(), true);
-
-            $request->request->replace(is_array($json_body) ? $json_body : []);
+                $request->request->replace(is_array($json_body) ? $json_body : []);
+            }
         }
     }
 }
