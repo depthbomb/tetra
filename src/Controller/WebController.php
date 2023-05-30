12d089cd7af151a6a4085bdefd7621d39edee987
depthbomb <reesepup77@hotmail.com>
2023-05-30 16:20:43 -0500

    feat: send early hints for index route

--- src/Controller/WebController.php
+++ src/Controller/WebController.php
@@ -1,6 +1,8 @@
 <?php namespace App\Controller;
 
+use App\Service\StaticService;
 use App\Attribute\RateLimited;
+use Symfony\Component\WebLink\Link;
 use App\Repository\ShortlinkRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -8,10 +10,12 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class WebController extends BaseController
 {
+    public function __construct(private readonly StaticService $static) {}
+
     #[Route('/', name: 'root')]
     public function serveSpa(): Response
     {
-        return $this->render('root/root.html.twig');
+        return $this->render('root/root.html.twig', response: $this->buildEarlyHints());
     }
 
     #[Route('/{shortcode}+', name: 'shortlink_expand', stateless: true)]
@@ -38,5 +42,26 @@ class WebController extends BaseController
         }
 
         return $this->redirectToRoute('root');
+    }
+
+    private function buildEarlyHints(): Response
+    {
+        $links = [];
+        foreach ([...$this->static->getEntries(), ...$this->static->getEntries('css')] as $asset)
+        {
+            $link = new Link(href: $asset);
+            if (str_ends_with($asset, 'js'))
+            {
+                $link->withAttribute('as', 'script');
+            }
+            else
+            {
+                $link->withAttribute('as', 'stylesheet');
+            }
+
+            $links[] = $link;
+        }
+
+        return $this->sendEarlyHints($links);
     }
 }
