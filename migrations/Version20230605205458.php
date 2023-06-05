61adef8d5c986b53d4fcff8736b9f54496abd0e1
depthbomb <reesepup77@hotmail.com>
2023-06-05 16:37:52 -0500

    refactor: remove messages table

--- migrations/Version20230605205458.php
+++ migrations/Version20230605205458.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230601020728 extends AbstractMigration
+final class Version20230605205458 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -26,21 +26,6 @@ final class Version20230601020728 extends AbstractMigration
         $this->addSql('CREATE INDEX IDX_C87C09461220EA6 ON shortlink (creator_id)');
         $this->addSql('CREATE TABLE "user" (id INT NOT NULL, username VARCHAR(36) NOT NULL, roles JSON NOT NULL, email VARCHAR(255) NOT NULL, sub VARCHAR(255) NOT NULL, api_key VARCHAR(255) NOT NULL, next_api_key_available VARCHAR(255) NOT NULL, created_at VARCHAR(255) NOT NULL, updated_at VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649F85E0677 ON "user" (username)');
-        $this->addSql('CREATE TABLE messenger_messages (id BIGSERIAL NOT NULL, body TEXT NOT NULL, headers TEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, available_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, delivered_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0 ON messenger_messages (queue_name)');
-        $this->addSql('CREATE INDEX IDX_75EA56E0E3BD61CE ON messenger_messages (available_at)');
-        $this->addSql('CREATE INDEX IDX_75EA56E016BA31DB ON messenger_messages (delivered_at)');
-        $this->addSql('COMMENT ON COLUMN messenger_messages.created_at IS \'(DC2Type:datetime_immutable)\'');
-        $this->addSql('COMMENT ON COLUMN messenger_messages.available_at IS \'(DC2Type:datetime_immutable)\'');
-        $this->addSql('COMMENT ON COLUMN messenger_messages.delivered_at IS \'(DC2Type:datetime_immutable)\'');
-        $this->addSql('CREATE OR REPLACE FUNCTION notify_messenger_messages() RETURNS TRIGGER AS $$
-            BEGIN
-                PERFORM pg_notify(\'messenger_messages\', NEW.queue_name::text);
-                RETURN NEW;
-            END;
-        $$ LANGUAGE plpgsql;');
-        $this->addSql('DROP TRIGGER IF EXISTS notify_trigger ON messenger_messages;');
-        $this->addSql('CREATE TRIGGER notify_trigger AFTER INSERT OR UPDATE ON messenger_messages FOR EACH ROW EXECUTE PROCEDURE notify_messenger_messages();');
         $this->addSql('ALTER TABLE shortlink ADD CONSTRAINT FK_C87C09461220EA6 FOREIGN KEY (creator_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 
@@ -53,6 +38,5 @@ final class Version20230601020728 extends AbstractMigration
         $this->addSql('ALTER TABLE shortlink DROP CONSTRAINT FK_C87C09461220EA6');
         $this->addSql('DROP TABLE shortlink');
         $this->addSql('DROP TABLE "user"');
-        $this->addSql('DROP TABLE messenger_messages');
     }
 }
