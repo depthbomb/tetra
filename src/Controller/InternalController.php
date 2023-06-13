e7055b4f46b55f9edd3a4d8323572ad8a56f5fa0
depthbomb <reesepup77@hotmail.com>
2023-06-12 23:52:03 -0500

    refactor: merge AdminController actions with InternalController

--- src/Controller/InternalController.php
+++ src/Controller/InternalController.php
@@ -1,17 +1,21 @@
 <?php namespace App\Controller;
 
+use App\Entity\Shortlink;
 use App\Attribute\CsrfProtected;
 use App\Repository\ShortlinkRepository;
 use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 #[Route('/api')]
 #[CsrfProtected('ajax')]
 class InternalController extends BaseController
 {
     public function __construct(
+        private readonly TranslatorInterface $translator,
         private readonly ShortlinkRepository $shortlinks,
         private readonly CacheInterface      $cache,
     ) {}
@@ -26,5 +30,47 @@ class InternalController extends BaseController
         });
 
         return $this->json(compact('count'));
+    }
+
+    #[Route('/admin/all-shortlinks', methods: ['POST'])]
+    public function getAllShortlinks(): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        $shortlinks = $this->shortlinks->createQueryBuilder('s')
+            ->leftJoin('s.creator', 'c')
+            ->select('s.shortcode, s.secret, s.destination, s.creator_ip, s.disabled, s.expires_at, s.created_at')
+            ->addSelect('c.id as user_id, c.username as user_username, c.sub as user_sub')
+            ->orderBy('s.created_at', 'DESC')
+            ->getQuery()
+            ->execute();
+
+        return $this->json($shortlinks);
+    }
+
+    #[Route('/admin/toggle-shortlink-disabled', methods: ['PATCH'])]
+    public function toggleShortlinkDisabled(Request $request): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        $payload = $request->getPayload();
+
+        $this->abortUnless($payload->has('shortcode'), 400, $this->translator->trans('error.shortlink.shortcode.missing'));
+
+        $shortcode = $payload->get('shortcode');
+        /** @var Shortlink $shortlink */
+        $shortlink = $this->shortlinks->createQueryBuilder('s')
+            ->where('s.shortcode = :shortcode')
+            ->setParameter('shortcode', $shortcode)
+            ->getQuery()
+            ->getOneOrNullResult();
+
+        $this->abortUnless(!!$shortlink, 404);
+
+        $shortlink->toggleDisabled();
+
+        $this->shortlinks->save($shortlink, true);
+
+        return $this->json([]);
     }
 }
