0b80d71d66617283c2bb3626a8f09e539fa16824
depthbomb <reesepup77@hotmail.com>
2023-06-16 22:56:50 -0500

    refactor: don't run HTML minifier in dev

--- src/EventSubscriber/HtmlMinifierSubscriber.php
+++ src/EventSubscriber/HtmlMinifierSubscriber.php
@@ -3,19 +3,23 @@
 use voku\helper\HtmlMin;
 use App\Util\Killswitch;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 
 #[AsEventListener(KernelEvents::RESPONSE, 'onKernelResponse', PHP_INT_MIN)]
 class HtmlMinifierSubscriber
 {
+    public function __construct(private readonly KernelInterface $kernel) {}
+
     public function onKernelResponse(ResponseEvent $event): void
     {
+        $dev      = $this->kernel->getEnvironment() === 'dev';
         $request  = $event->getRequest();
         $response = $event->getResponse();
         $route    = $request->attributes->getString('_route');
 
-        if (!$event->isMainRequest() or $route !== 'root' or !Killswitch::RENDERED_HTML_MINIFICATION_ENABLED)
+        if (!$event->isMainRequest() or $dev or $route !== 'root' or !Killswitch::RENDERED_HTML_MINIFICATION_ENABLED)
         {
             return;
         }
