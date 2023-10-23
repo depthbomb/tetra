e24879820acf68f79d614843651f588cdcd6b813
depthbomb <depthbomb@users.noreply.github.com>
2023-10-22 23:00:45 -0500

    refactor: simplify JSON response helper

--- apps/server/src/utils/response.ts
+++ apps/server/src/utils/response.ts
@@ -4,10 +4,5 @@ import type { Context } from 'koa';
 export function sendJsonResponse(ctx: Context, data: unknown, statusCode: number = 200): void {
 	ctx.response.status = statusCode;
 	ctx.response.set('Content-Type', 'application/json');
-
-	if (Features.isEnabled('PRETTY_PRINT_JSON')) {
-		data = JSON.stringify(data, null, 4);
-	}
-
-	ctx.response.body = data;
+	ctx.response.body = JSON.stringify(data, null, Features.isEnabled('PRETTY_PRINT_JSON') ? 4 : 0);
 }
