fc51d2efd5a32e8c2ea474eedb200afc1988a045
depthbomb <depthbomb@users.noreply.github.com>
2023-10-24 13:31:45 -0500

    refactor: update API spec

--- apps/server/src/views/swagger.ts
+++ apps/server/src/views/swagger.ts
@@ -3,26 +3,23 @@ import type { Context } from 'koa';
 
 const template = `
 <!doctype html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <title>go.super.fish API Documentation</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui.min.css">
-    </head>
-    <body>
-        <div id="swagger"></div>
-        <script src="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui-bundle.min.js" defer></script>
-        <script type="text/javascript">
-            window.onload = () => {
-                window.ui = SwaggerUIBundle({
-                    url: '/api/openapi.json',
-                    dom_id: '#swagger'
-                });
-            }
-        </script>
-    </body>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>go.super.fish API Documentation</title>
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui.min.css">
+	</head>
+	<body>
+		<div id="swagger"></div>
+		<script src="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui-bundle.min.js" defer></script>
+		<script type="text/javascript">
+			window.onload = () => {
+				window.ui = SwaggerUIBundle({ url: '/api/openapi.json', dom_id: '#swagger' });
+			}
+		</script>
+	</body>
 </html>
 `.trim();
 
